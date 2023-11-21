2027f907b0cd39c626becddb9e520fea8e767b33
Sandesh Bageshwar <sandesh16bageshwar@gmail.com>
2023-11-21 14:05:48 +0530

    Update Sum.java

--- Sum.java
+++ Sum.java
@@ -1,12 +1,12 @@
 import java.util.Scanner;
 public class Sum{
 	public static void main(String args[]){
-		int x,y,sum;
+		int x=10,y=20,sum;
 		Scanner in = new Scanner(System.in);
-		System.out.println("Enter the first number");
-		x= in.nextInt();
-		System.out.println("Enter the second number");
-		y= in.nextInt();
+		System.out.println("Enter the first number"+ x);
+		
+		System.out.println("Enter the second number"+ y);
+		
 		sum=x+y;
 		System.out.println("sum is " + sum);
 	}
